90aca05374c556c1961618117b6f7d24d62e5706
Nataly <ladarunataly99@gmail.com>
2021-06-11 02:02:46 +0300

    Task 2 - Done

---
+++ actions.py
@@ -0,0 +1,21 @@
+import csv
+
+class Actions:
+
+    def __init__(self):
+        self.data = None
+
+
+    def create_json_obj(array):
+        return {
+            array[0]:{
+                'id': array[1],
+                'name': array[2],
+                'provisional': array[3],
+                'country': array[4],
+                'url': array[5],
+                'image': array[6],
+                'pdf': array[7]
+            }
+        }
+
--- app.py
+++ app.py
@@ -1,9 +1,22 @@
-from flask import Flask
+from flask import Flask,request
+from actions import Actions
 app=Flask(__name__)
+
 
 @app.route("/home")
 def hello():
     return "Welcome!", 200
+
+@app.route("/dog-list", methods=['GET','POST','DELETE'])
+def dog_list():
+    if request.method == 'GET':
+        pass
+
+    elif request.method == 'POST':
+        pass
+
+    elif request.method == 'DELETE':
+        pass
 
 if __name__ == '__main__':
     app.run()
